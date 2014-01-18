6d0f2118915700b20c10c0c4804ec956026f5c51
Bill Bradley <bill@bill-bradley.com>
2014-01-18 13:36:03 -0500

    Added north, east, south, west tile properties

--- PacMan/Tile.cpp
+++ PacMan/Tile.cpp
@@ -1,8 +1,11 @@
 #include "stdafx.h"
 #include "Tile.h"
 
-Tile::Tile()
+Tile::Tile(int n, int e, int s, int w)
 {
-
+	north = n;
+	east = e;
+	south = s;
+	west = w;
 }
 
--- PacMan/Tile.h
+++ PacMan/Tile.h
@@ -3,16 +3,35 @@
 #ifndef _TILE_
 #define _TILE_
 
-#define TILE_HEIGHT 32
-#define TILE_WIDTH 32
+const int TILE_HEIGHT = 32;
+const int TILE_WIDTH = 32;
+const int TOTAL_TILES = 12;
+
+// Tiles
+const int TILE_EMPTY = 0;
+const int TILE_SW = 1;
+const int TILE_EW = 2;
+const int TILE_ES = 3;
+const int TILE_ESW = 4;
+const int TILE_NW = 5;
+const int TILE_NS = 6;
+const int TILE_NSW = 7;
+const int TILE_NE = 8;
+const int TILE_NEW = 9;
+const int TILE_NES = 10;
+const int TILE_NESW = 11;
 
 class Tile
 {
 public:
-	Tile();
+	Tile(int n, int e, int s, int w);
+	void Draw();
 
  private:
-	void Draw();
+	 int north;
+	 int east;
+	 int south;
+	 int west;
 };
 
 #endif // _TILE_
