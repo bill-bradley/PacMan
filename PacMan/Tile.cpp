bedcd5efa527f8739f25b8c07e6c5bfcd3e51926
Bill Bradley <bill@bill-bradley.com>
2014-01-18 14:49:07 -0500

    Added tiles

--- PacMan/Tile.cpp
+++ PacMan/Tile.cpp
@@ -1,11 +1,13 @@
 #include "stdafx.h"
 #include "Tile.h"
 
-Tile::Tile(int n, int e, int s, int w)
+Tile::Tile(int smi, int n, int e, int s, int w)
 {
 	north = n;
 	east = e;
 	south = s;
 	west = w;
+	spriteMapIndex = smi;
+
 }
 
--- PacMan/Tile.h
+++ PacMan/Tile.h
@@ -1,37 +1,21 @@
 #include "stdafx.h"
+#include <SFML/Graphics.hpp>
 
 #ifndef _TILE_
 #define _TILE_
 
-const int TILE_HEIGHT = 32;
-const int TILE_WIDTH = 32;
-const int TOTAL_TILES = 12;
-
-// Tiles
-const int TILE_EMPTY = 0;
-const int TILE_SW = 1;
-const int TILE_EW = 2;
-const int TILE_ES = 3;
-const int TILE_ESW = 4;
-const int TILE_NW = 5;
-const int TILE_NS = 6;
-const int TILE_NSW = 7;
-const int TILE_NE = 8;
-const int TILE_NEW = 9;
-const int TILE_NES = 10;
-const int TILE_NESW = 11;
-
 class Tile
 {
 public:
-	Tile(int n, int e, int s, int w);
-	void Draw();
+	Tile(int smi, int n, int e, int s, int w);
 
  private:
+	 int spriteMapIndex;
 	 int north;
 	 int east;
 	 int south;
 	 int west;
+
 };
 
 #endif // _TILE_
