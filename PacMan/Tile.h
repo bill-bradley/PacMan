455770a7634723811762cf1f70e4ee524aef855c
Bill Bradley <bill@bill-bradley.com>
2014-01-18 12:35:42 -0500

    Add initial Tile class

---
+++ PacMan/Tile.h
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+
+#ifndef _TILE_
+#define _TILE_
+
+#define TILE_HEIGHT 32
+#define TILE_WIDTH 32
+
+class Tile
+{
+public:
+	Tile();
+
+ private:
+	void Draw();
+};
+
+#endif // _TILE_
